59759501b84d1efa26cdcf2dc8daecae7f954109
Antti Leppä <antti.leppa@metatavu.fi>
2021-08-17 11:11:08 +0300

    Fixed language tests

--- src/native-test/kotlin/fi/metatavu/edufication/pages/api/test/functional/tests/NativeLanguagesTestIT.kt
+++ src/native-test/kotlin/fi/metatavu/edufication/pages/api/test/functional/tests/NativeLanguagesTestIT.kt
@@ -3,6 +3,7 @@ package fi.metatavu.edufication.pages.api.test.functional.tests
 import fi.metatavu.edufication.pages.api.test.functional.resources.KeycloakTestResource
 import fi.metatavu.edufication.pages.api.test.functional.resources.LocalTestProfile
 import fi.metatavu.edufication.pages.api.test.functional.resources.MysqlTestResource
+import fi.metatavu.edufication.pages.api.test.functional.resources.S3TestResource
 import io.quarkus.test.common.QuarkusTestResource
 import io.quarkus.test.junit.NativeImageTest
 import io.quarkus.test.junit.TestProfile
@@ -15,6 +16,7 @@ import io.quarkus.test.junit.TestProfile
 @NativeImageTest
 @QuarkusTestResource.List(
     QuarkusTestResource(KeycloakTestResource::class),
+    QuarkusTestResource(S3TestResource::class),
     QuarkusTestResource(MysqlTestResource::class)
 )
 @TestProfile(LocalTestProfile::class)
